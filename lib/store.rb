022688da1b0147b5b0d9cf74160aea601491109d
Taylor <thulsmans.2994@me.com>
2018-02-27 18:56:59 +0000

    exercies 9

--- lib/store.rb
+++ lib/store.rb
@@ -3,12 +3,20 @@ class Store < ActiveRecord::Base
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: {greater_than: 0}
   validate :has_man_or_woman
+  before_destroy :check_for_employees
 
   private
   def has_man_or_woman
     if mens_apparel != true && womens_apparel != true
       errors.add(:mens_apparel, "must have gender binary")
       errors.add(:womens_apparel, "must have genderbinary")
+    end
+  end
+
+  def check_for_employees
+    if self.employees.size > 1
+      errors[:base] << "Cannot delete a store with employees"
+      throw :abort
     end
   end
 end
