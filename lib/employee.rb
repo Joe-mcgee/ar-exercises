99fd0f7b88607589486d7533674e6cf46e84e555
Taylor <thulsmans.2994@me.com>
2018-02-27 04:31:03 +0000

    adds an auto generating password for employees

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,17 @@
 class Employee < ActiveRecord::Base
-  belongs_to :store
+  belongs_to :stores
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :hourly_rate, :inclusion => {:in => [40,200]}
+  validates :hourly_rate, :inclusion => {:in => 40..200}
   validates :store_id, presence: true
+
+  before_create do
+    self.password = gen_password
+  end
+  private
+  def gen_password
+    randomStr = (0...8).map { ('a'..'z').to_a[rand(26)]}.join
+    puts randomStr
+    return randomStr
+  end
 end
