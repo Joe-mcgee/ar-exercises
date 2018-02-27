fb77fc6ef5ce136cf77209960e41d89ffa8d7ada
Taylor <thulsmans.2994@me.com>
2018-02-27 01:30:09 +0000

    exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,18 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+@sum = Store.sum('annual_revenue')
+
+puts @sum
+
+@number = Store.count
+
+puts @sum/@number
+
+
+@rich = Store.where('annual_revenue > 1000000')
+
+@rich.each do |store|
+  puts "#{store.name}"
+end
